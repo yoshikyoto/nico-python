5f8bf1d4ad0e17e7b17f3fb823f2627afd074142
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-04-27 11:12:16 +0900

    comment dataset download

---
+++ src/nico/repository/comment_repository.py
@@ -0,0 +1,24 @@
+import os.path
+import urllib.request
+
+class CommentRepository():
+
+    __archive_base_url = "http://dlsv.dsc.nii.ac.jp/idr/stb_yoshiyuki-s@hotmail.co.jp/158c402d9e8c6f17/nicocomm/data.20161216/comment/"
+
+    def __init__(self, base_dir):
+        self.base_dir = base_dir
+
+    def download_archive(self, filename):
+        filepath = self.base_dir + filename
+        if os.path.isfile(filepath):
+            print("File already exists")
+            return True
+
+        url = self.__archive_base_url + filename
+        try:
+            print("Download: " + url + "to" + filepath)
+            urllib.request.urlretrieve(url, filepath)
+            return True;
+        except urllib.error.HTTPError:
+            print("No Archive")
+            return False;

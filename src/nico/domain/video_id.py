4792681f05b1e6775796211268384f633684dde1
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-04-27 10:55:13 +0900

    add get archive name method video_id

--- src/nico/domain/video_id.py
+++ src/nico/domain/video_id.py
@@ -1,3 +1,5 @@
+import math
+
 class VideoId():
 
     def __init__(self, id):
@@ -5,3 +7,6 @@ class VideoId():
 
     def get_number(self):
         return int(self.__id[2:])
+
+    def get_comment_dataset_archive_name(self):
+        return str(math.floor(self.get_number() / 10000))
